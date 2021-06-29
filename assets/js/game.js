9d4d6acf60be6f446016b4cabde57e4bce7af956
jonestechics <mrmckinley04@gmail.com>
2021-06-28 18:14:16 -0600

    start and end game funtions

--- assets/js/game.js
+++ assets/js/game.js
@@ -94,3 +94,52 @@ for (var i = 0; i < enemyNames.length; i++) {
     break;
   }
 }
+// function to start a new game
+var startGame = function() {
+  //reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+
+  // other logic remains the same...
+};
+  for (var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+      window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+      var pickedEnemyName = enemyNames[i];
+
+      enemyHealth = 50;
+
+      fight(pickedEnemyName);
+    }
+    else {
+      window.alert("You have lost your robot in battle! Game Over!");
+      break;
+    }
+  }
+
+//start the game when the page loads
+//startGame();
+
+// function to end the entire game
+var endGame = function() {
+  // if player is still alive, player wins!
+  if (playerHealth > 0) 
+  window.alert("Great job, you've survived the game! you now have a score of " + playerMoney + ".");
+
+else {
+  window.alert(" You've lost your robot in battle.");
+  }
+};
+// ask player if they'd like to play again
+var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+  //restart the game
+  startGame();
+}
+else {
+  window.alert("Thanks you for playing Robot Gladiators! Come back soon!");
+}
+endGame();
