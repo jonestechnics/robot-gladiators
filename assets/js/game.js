e4b21ede74730ecdb9897bd567ef060aa52074e1
jonestechics <mrmckinley04@gmail.com>
2021-07-10 21:54:03 -0600

    shop updates

--- assets/js/game.js
+++ assets/js/game.js
@@ -181,7 +181,6 @@ var shop = function() {
   // ask player what they'd like to do
   var shopOptionPrompt = window.prompt('Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one "REFILL", "UPGRADE", or "LEAVE" to make a choice.'
   );
-
   // use switch to carry out action
   switch (shopOptionPrompt) {
     case 'REFILL': // new case
